b5d14299b846b721dfa6eb4000c38472537989e3
peter <parkkw09@kakaovx.com>
2020-01-16 18:30:59 +0900

    Modified very simple content.

---
+++ app/src/main/java/app/peterkwp/customlayout1/api/GlideModule.kt
@@ -0,0 +1,7 @@
+package app.peterkwp.customlayout1.api
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule: AppGlideModule()
